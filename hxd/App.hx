6619e8e7c2bde8cebda98bbb2492d4d9bc2abe84
Greg Caldwell <caldwellgp@googlemail.com>
2022-08-05 11:54:18 +0100

    OpenFL MouseOut event fixes

--- hxd/App.hx
+++ hxd/App.hx
@@ -191,6 +191,9 @@ class App implements h3d.IDrawable {
 
 	function mainLoop() {
 		hxd.Timer.update();
+		#if openfl
+		@:privateAccess if (sevents.pendingEvents.length > 0)
+		#end
 		sevents.checkEvents();
 		if( isDisposed ) return;
 		update(hxd.Timer.dt);
