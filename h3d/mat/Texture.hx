f51be82cd2bec9e4a26d2461f0462aa332abfa15
guruas3 <you@example.com>
2025-05-29 20:47:18 +0200

    Allow to bypass cache in Texture.fromColor

--- h3d/mat/Texture.hx
+++ h3d/mat/Texture.hx
@@ -438,14 +438,16 @@ class Texture {
 	/**
 		Creates a 1x1 texture using the RGB color passed as parameter.
 	**/
-	public static function fromColor( color : Int, ?alpha = 1. ) {
+	public static function fromColor( color : Int, ?alpha = 1., ?bypassCache: Bool = false ) {
 		var engine = h3d.Engine.getCurrent();
 		var aval = Std.int(alpha * 255);
 		if( aval < 0 ) aval = 0 else if( aval > 255 ) aval = 255;
 		var key = (color&0xFFFFFF) | (aval << 24);
-		var t = @:privateAccess engine.textureColorCache.get(key);
-		if( t != null )
-			return t;
+        if ( !bypassCache ) {
+            var t = @:privateAccess engine.textureColorCache.get(key);
+            if ( t != null )
+                return t;
+        }
 		var t = new Texture(1, 1, null);
 		t.clear(color, alpha);
 		t.realloc = function() t.clear(color, alpha);
